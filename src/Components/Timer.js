2ed1bbd41b537eb14175722f9b17cf9cce2b1cf3
Joao Victor <imashinzo10@gmail.com>
2022-11-17 21:57:25 -0300

    Adicionado proptypes ao elemento timer

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Timer extends React.Component {
   componentDidMount() {
@@ -22,5 +23,12 @@ class Timer extends React.Component {
     );
   }
 }
+
+Timer.propTypes = {
+  time: PropTypes.number.isRequired,
+  isAnswered: PropTypes.bool.isRequired,
+  intervalId: PropTypes.number.isRequired,
+  updateTimer: PropTypes.func.isRequired,
+};
 
 export default Timer;
