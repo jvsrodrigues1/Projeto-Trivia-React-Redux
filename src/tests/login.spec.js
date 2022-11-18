f9b9352cf91802f95930f0735ac4ad922ca1d457
Joao Victor <imashinzo10@gmail.com>
2022-11-17 21:45:42 -0300

    Iniciando os testes em Login.js

--- src/tests/login.spec.js
+++ src/tests/login.spec.js
@@ -1,7 +1,39 @@
 import React from "react";
-import App from "../App";
-import tokenResponse from "./mocks/token";
-import questionsResponse from "./mocks/questions";
 import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
 import { screen, waitFor, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import Login from "../pages/Login";
+import App from "../App";
+import tokenResponse from "./mocks/token";
+import questionsResponse from "./mocks/questions";
+
+describe("Verifica a página de Login", () => {
+  test("Verifica se os inputs são renderizados na tela", () => {
+    renderWithRouterAndRedux(<Login />);
+
+    const name = screen.getByTestId("input-player-name");
+    const email = screen.getByTestId("input-gravatar-email");
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+
+    expect(name).toHaveValue("");
+    expect(email).toHaveValue("");
+  });
+
+  test("Verifica se o botão é habilitado após preencher os inputs", () => {
+    renderWithRouterAndRedux(<Login />);
+
+    const button = screen.getByTestId("btn-play");
+
+    expect(button).toBeDisabled();
+
+    const name = screen.getByTestId("input-player-name");
+    const email = screen.getByTestId("input-gravatar-email");
+
+    userEvent.type(name, "meunome");
+    userEvent.type(email, "meuemail");
+
+    expect(button).toBeEnabled();
+  });
+});
